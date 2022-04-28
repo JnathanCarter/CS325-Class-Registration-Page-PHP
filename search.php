a15c76555082f56a757c26061983b34588d37674
JnathanCarter <jcarter20@stu.jsu.edu>
2022-04-28 00:56:26 -0500

    Co-authored-by: Maurzel  <realmotty@users.noreply.github.com>

--- search.php
+++ search.php
@@ -9,41 +9,34 @@
 </head>
 
 <body>
-        <?php
-        error_reporting(E_ALL);
-        ?>
-        <?php
-        ini_set('error_reporting', E_ALL);
-        ?>
-        <?php
-        function contstructTime($hours, $minute)
+        <?php function contstructTime($hours, $minute)
         {
                 return "$hours:$minute:00";
         }
+
         $changedValues = [];
+
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 echo "<p>This is a GET request.</p>\n";
         } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
                 $beginHour = $_POST['begin_hh'];
                 $beginMinute = $_POST['begin_mm'];
+
                 $endHour = $_POST['end_hh'];
                 $endMinute = $_POST['end_mm'];
                 $beginAP = $_POST['begin_ap'];
-                $endAP = $_POST['end_ap'];
 
+                $endAP = $_POST['end_ap'];
                 $beginTimechange = false;
                 $endTimechange = false;
-
-
-
-                //adds values to changedValue array if the values are not defaults
+                //adds values to changedValuearray if the values are not defaults
                 foreach ($_POST as $key => $vale) {
 
                         if ($vale != "default" && $vale != "") {
                                 echo "<p>Changed Key: $key, Changed Value: $vale </p>";
 
-                                $changedValues[$key] =  $vale;
+
+                                $changedValues[$key] = $vale;
                         }
                 }
 
@@ -52,19 +45,22 @@
 
                         $beginTimechange = true;
 
+
                         if ($changedValues["begin_ap"] == "pm") {
                                 $hours = $changedValues["begin_hh"] + 12;
+
 
                                 $beginFormatted = $hours . ":" . $changedValues["begin_mm"] . ":00";
                         } else {
                                 $beginFormatted = $changedValues["begin_hh"] . ":" . $changedValues["begin_mm"] . ":00";
                         }
 
-                        $changedValues["formattedBegin"] = $beginFormatted;
+                        $changedValues["start"] = $beginFormatted;
                 }
                 if (array_key_exists("end_hh", $changedValues)) {
                         if ($changedValues["end_ap"] == "pm") {
                                 $hours = $changedValues["end_hh"] + 12;
+
 
                                 $endFormatted = $hours . ":" . $changedValues["end_mm"] . ":00";
                         } else {
@@ -72,36 +68,93 @@
                         }
 
 
-                        $changedValues["formattedEnd"] = $endFormatted;
+                        $changedValues["end"] = $endFormatted;
                 }
+                //remove individual time fields
+                $removeBegin = ['begin_hh', 'begin_mm', 'begin_ap'];
+
+                foreach ($removeBegin as $key) {
+                        unset($changedValues[$key]);
+                }
+                $removeEnd = ['end_hh', 'end_mm', 'end_ap'];
 
 
+                foreach ($removeEnd as $key) {
+                        unset($changedValues[$key]);
+                }
+                //if days is set to any remove days
 
+                if ($changedValues['days'] == "any") {
 
-                echo "<p>begin chagne ------$beginTimechange";
-                echo "<p>end change ------------- $endTimechange";
+                        unset($changedValues['days']);
+                }
                 //diagnostic print
-
                 foreach ($changedValues as $key => $vale) {
                         echo "<p> $key ---------- $vale</p>";
                 }
 
-                function removeBeginEndHHMM()
-                {
-                }
 
-                function convertToFormatted24HHTime($hours, $minutes, $meridian)
-                {
-                        $result = "";
-                        if ($meridian == "pm") {
-                                $hours = $hours + 12;
-                                $minutes = $minutes + 12;
-                                $result = $hours + ":" + $minutes + ":00";
-                        } else {
-                                $result = $hours + ":" + $minutes + ":00";
-                        }
-                        return $result;
+
+                $cURL = curl_init();
+
+                $url = "http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS325_Project3/search";
+
+                $url = $url . '?' . http_build_query($changedValues);
+
+                curl_setopt($cURL, CURLOPT_URL, $url);
+                // Set URL
+                curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+                // Return Transfer as String
+                $output = curl_exec($cURL);
+                // Execute Transfer (returns JSON)
+                curl_close($cURL);
+                // Close cURL Session
+                echo "<p>";
+                // Begin Output Paragraph
+                $json = json_decode($output, true);
+                // Decode JSON String
+                foreach ($json as $key => $value) { // Print JSON Key/Value Pairs
+                        echo "$key: " . var_export($value, true) . "<br />";
                 }
+                echo "</p>
+                <p>";
+                // Begin New Output Paragraph
+                $jsonString = json_encode($json);
+                // Encode JSON Array to a String
+                echo $jsonString;
+                // Print Encoded JSON String
+                echo "</p>";
+
+                /*
+                echo "<p>url--------------$url";
+                
+                        curl_setopt($cURL, CURLOPT_URL, $url);
+                        
+                        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+                        
+                        $output = curl_exec($cURL);
+                        
+                        curl_close($cURL);
+                        
+                        echo "
+                <p>";
+                
+                        $json = json_decode($output, true);
+                        
+                        foreach ($json as $key => $value) {
+                        echo "$key: " . var_export($value, true) . "<br />";
+                        
+                        }
+                        echo "</p>
+                <p>";
+                
+                        $jsonString = json_encode($json);
+                        
+                        echo $jsonString;
+                        
+                        echo "</p>";
+                        
+                */
         }
 
 
